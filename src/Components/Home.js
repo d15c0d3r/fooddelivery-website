100ec9b9100ca0f40c80c5e0a8f461002cf0ea29
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-05 22:27:50 +0530

    1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import Login from "./HomeComponents/Login";
 import Signup from "./HomeComponents/Signup";
-import ForgotPass from "./HomeComponents/ForgotPass";
 import Nav from "./Nav";
 import {useEffect} from "react"
 import Cookies from "js-cookie";
@@ -14,7 +13,6 @@ function Home(){
             <Nav/>
             <Login/>
             <Signup/>
-            <ForgotPass/>
         </div>
     );
 }
