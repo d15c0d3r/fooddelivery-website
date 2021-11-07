2ebfe02d678b2946d7bdb03ed8d9bb9bc46dcd73
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-07 20:11:31 +0530

    protectedroute

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,25 +1,12 @@
 import { Route,Redirect, useHistory } from "react-router"
 import Cookies from "js-cookie"
-import axios from "axios"
-import {useState,useEffect} from "react"
+
 
 function ProtectedRoute({component : Component, ...rest}){
-    const [authenticated,setAuthenticated] = useState(false)
-    const history = useHistory()
-
-    useEffect(()=>{
-        const token = Cookies.get("token")
-        if(token){
-            setAuthenticated(true)
-        }
-        else{
-            setAuthenticated(false)
-        }
-    })
     return(
         <Route
-            {...rest}
-            render = {(props)=> Cookies.get("token")? <Component{...props}/> : <Redirect to = "/"/>}            
+            {...rest} 
+            render = {(props)=> Cookies.get("token")? <Component{...props}/> : <Redirect to = "/"/>}              
         />
     )
 }
