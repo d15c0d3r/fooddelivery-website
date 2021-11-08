1bf0e45a84ba215a82ec9228751010fec719a554
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-08 15:03:00 +0530

    removed : state : loggedIn : Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,19 +3,17 @@ import {useEffect, useState} from "react"
 import Cookies from "js-cookie"
 import Booking from "./HomeComponents/Booking"
 import Signup from "./HomeComponents/Signup"
-import CheckLoggedIn from "../Apis/CheckLoggedIn"
 
-function Home(){
-    const [logged,setLogged] = useState(false)
+function Home(props){
     const [signed, setSigned] = useState(false)
 
     useEffect(()=>{
         Cookies.remove("orders")
-        CheckLoggedIn(setLogged)
     },[])
+    
     return(
         <div>
-            {!logged?<div>
+            {!props.loggedIn?<div>
                         <Login/>
                         <br/>
                         {!signed?<Signup handleSigned = {(val)=>setSigned(val)}/>:<div>Login to continue</div>}
