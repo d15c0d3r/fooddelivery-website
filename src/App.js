cbb4a5649ff263d9b116b1775e797226047910f5
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-08 14:04:33 +0530

    component : added : PageNotFound

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {Route , Switch} from "react-router-dom"
 import Home from "./Components/Home"
-import Booking from "./Components/HomeComponents/Booking"
 import Daffodills from "./Components/Daffodills"
 import Foodcourt from "./Components/Foodcourt"
 import Checkout from "./Components/CheckoutComponents/Checkout"
 import ForgotPass from "./Components/HomeComponents/ForgotPass"
 import Nav from "./Components/Nav"
 import ProtectedRoute from "./ProtectedRoute"
+import PageNotFound from "./Components/PageNotFound"
 
 function App() {
   return(
@@ -18,6 +18,7 @@ function App() {
         <ProtectedRoute exact path = "/bookings/food-court" component = {Foodcourt}></ProtectedRoute>
         <ProtectedRoute exact path = "/checkout" component = {Checkout}></ProtectedRoute>
         <Route exact path = "/forgot-password"><ForgotPass/></Route>
+        <Route><PageNotFound/></Route>
       </Switch>
     </div>
   )
