71c814d832d17d54241fa581d00009621854acc3
thatviper <challanaganarasimhareddy@gmail.com>
2021-11-08 15:03:24 +0530

    Modifited : a tag to Link tag : Nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,21 +1,27 @@
 import Cookies from "js-cookie"
-import {useHistory} from "react-router-dom"
+import {useHistory,Link} from "react-router-dom"
 
-function Nav(){
+function Nav(props){
     const history = useHistory()
 
     function handleLogout(e){
-        Cookies.remove("orders")
+        props.handleLoggedIn(false)
         Cookies.remove("token")
         Cookies.remove("email")
         history.push("/")
     }
     return (
         <div>
-            {Cookies.get("email")?<h2>{Cookies.get("email")}</h2> : <div></div>}
-            <a href="http://localhost:3000/">Home</a>
-            
-            {(Cookies.get("token"))? (<div><a href="http://localhost:3000/" onClick = {(e)=>handleLogout(e)}>Logout</a></div>) : (<br/>)}
+        <ul>
+            {Cookies.get("email")?
+                <li>
+                    <h2>{Cookies.get("email")}</h2>
+                </li> : null}
+            <li>
+                <Link to = "/">Home</Link>
+            </li>
+            {props.loggedIn? <li><a href="http://localhost:3000/" onClick = {(e)=>handleLogout(e)}>Logout</a></li> :null}
+        </ul>    
             <br/>
         </div>
     )
